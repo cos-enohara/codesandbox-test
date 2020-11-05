00120ec5a0037075f33e4dddd9e5e5ff06038f23
cos-enohara <59213462+cos-enohara@users.noreply.github.com>
2020-11-05 17:59:50 +0900

    アロー関数まで

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import "./styles.css";
+// var・・・再宣言、書き換え可能
+// let・・・再宣言不可、書き換え可能
+// const・・・再宣言不可、書き換え不可。ただし、リストのようなオブジェクトは書き換え可能
 
-document.getElementById("app").innerHTML = `
+// const val4 = {
+//   name: "榎原",
+//   age: "25"
+// };
+// val4.name = "山下";
+// val4.address = "東京";
+// console.log(val4);
 
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
-console.log("aaaaaaaaa");
+//テンプレート文字列
+// const name = "榎原";
+// const age = 25;
+
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// アロー関数
+/**
+ * 従来の関数
+ * function func1(str) {
+ *  return str;
+ * }
+ */
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+//アロー関数
+const func2 = (str) => str;
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(func3(10, 20));
